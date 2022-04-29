54ae9fb9228da57303df09bcda0850300a2f3ac8
DeNiro Dumas <denirod@Deniros-MBP.attlocal.net>
2022-04-29 08:23:12 -0400

    updated routes

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 // parse incoming JSON data
 app.use(express.json());
 const { animals } = require('./data/animals.json')
@@ -104,8 +105,21 @@ app.post('/api/animals', (req, res) => {
 
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+})
 
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`)
